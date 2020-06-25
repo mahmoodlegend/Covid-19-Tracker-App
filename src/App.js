68e257e6d9e6f4fad1d10d400b9b2f231db44706
mohammadmahmood441@gmail.com <m.mahmood8176@gmail.com>
2020-06-25 16:36:11 +0500

    Three Cards Prepard

--- src/App.js
+++ src/App.js
@@ -12,23 +12,32 @@ import styles from './App.module.css';
 import { fetchData } from './api/index.js';    
 
                             //We install some of components with   
-// Command: npm install --save axios react-chartjs-2 react-countup classnames
+// Command: npm install --save axios react-chartjs-2 react-countup classnames @material-ui/core
 // 1) axios we get request to api 
 // 2) react-chartjs-2 for making chart
 // 3) react-countup for making number count for animation
+// 4) @material-ui/core
 class App extends React.Component {
-    
+       
+        state = {                      // Now we are sending data to cards so that make UI with this data
+                                // State is built in function for handle data
+            data : {},                 // It Still Empty Object Initalized
+        }       
 
     async componentDidMount() {
-        const data = await fetchData();
-        console.log(data);
+        const fetchedData = await fetchData();    // fetchedData word give sense that data is coming in this variable
+        
+        this.setState({data:fetchData});    // data:data can be written as only data in JS as key value pair are same but just for clearity     
     }
 
     render() {
+
+        const { data } = this.state;
+
         return (
             <div div className={styles.container}>
                 {/* Same as className = container  , It make sure that you not have interface fromany other file  */}
-                <Cards />
+                <Cards data = {data} />        {/* We pass all data to card Function */}
                 <CountryPicker />
                 <Chart />
             </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import styles from './Cards.module.css';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';   // These are the elements imported from material-ui, These will be helpful for making of cards
 
-const Cards = () => {
-    return(
-        <h1>Cards</h1>
+
+const Cards = (props) => {
+    console.log(props);           // To See Either We Are Getting Data or not So data was come but was not shown so we need three component here
+
+    return (
+        <div className={styles.container}>                  {/* Jsx strutute */}
+
+            <Grid container spacing={3} justify="center">     {/* Making Cards location on page and spacing b/w them */}
+
+                <Grid item component={Card}>                {/* This will Make Card Look like paper */}
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>    {/*Use for Write Text in Card, gutterBottom Provide Padding on Bottom*/}
+                        <Typography varaint="h5">REAL DATA</Typography>             {/*varaint is for text size*/}
+                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography varaint="body2">Number of Active Cases of COVID-19</Typography>  {/*varaint is for text size*/}
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>                {/* This will Make Card Look like paper */}
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>    {/*Use for Write Text in Card, gutterBottom Provide Padding on Bottom*/}
+                        <Typography varaint="h5">REAL DATA</Typography>             {/*varaint is for text size*/}
+                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography varaint="body2">Number of recoveries from COVID-19</Typography>  {/*varaint is for text size*/}
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>                {/* This will Make Card Look like paper */}
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>    {/*Use for Write Text in Card, gutterBottom Provide Padding on Bottom*/}
+                        <Typography varaint="h5">REAL DATA</Typography>             {/*varaint is for text size*/}
+                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography varaint="body2">Number of Deaths Cases by Crona Virus</Typography>  {/*varaint is for text size*/}
+                    </CardContent>
+                </Grid>
+
+            </Grid>
+
+        </div>
     )
 }
 
